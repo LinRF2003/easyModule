a3df9b6985bb7ae6eb094c1b2b8e5cec34239498
LinRF <2673760057@qq.com>
2024-10-23 14:44:42 +0800

    添加面包屑组件

---
+++ src/renderer/src/components/Breadcrumb.vue
@@ -0,0 +1,10 @@
+<template>
+  <el-breadcrumb :separator-icon="ArrowRight">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item><slot></slot></el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script lang="ts" setup>
+import { ArrowRight } from '@element-plus/icons-vue'
+</script>
--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -5,8 +5,13 @@ import router from './router/router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import Breadcrumb from '@/components/Breadcrumb.vue'
+
 const app = createApp(App)
 
 app.use(router)
 app.use(ElementPlus)
+
+app.component('Breadcrumb', Breadcrumb)
+
 app.mount('#app')
